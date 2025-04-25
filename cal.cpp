37ee865c213047ad2556050588214a5c65c68ac1
mnmaldo <mnmaldonado2@espe.edu.ec>
2025-04-25 10:35:35 -0500

    Add division function

--- cal.cpp
+++ cal.cpp
@@ -6,3 +6,63 @@ int main(){
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void division (){
+float n1, n2;
+cout<<"Primer numero:"<<endl;
+cin n1;
+cout<<"Segundo numero;"<<endl;
+cin n2;
+if (b!=0){
+    cout<<"Resultado: "<<n1/n2<<endl;
+}
+else {
+    cout<<"No se divide entre 0"<<endl;
+}
+}
+
+
+
+
+
+
+
+
