86915ab8f7d584dba5b18ad002543cb868eb00eb
calleluis <lfcalle1@espe.edu.ec>
2025-04-25 10:31:52 -0500

    add multiplicacion function

--- cal.cpp
+++ cal.cpp
@@ -6,3 +6,40 @@ int main(){
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void multiplicacion(){
+int n1,n2,resultado;
+
+cout<<"Ingrese el primer numero: "<<endl;
+cin>>n1;
+cout<<"Ingrese el segundo numero: "<<endl;
+cin>>n2;
+resultado = n1*n2;
+cout<<"La multiplicacion de los numeros es: "<<resultado<<endl;
+}
