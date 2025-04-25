a215766a11134824a6b453c31acf704908deed50
MadellynTC <matasipanta@espe.edu.ec>
2025-04-25 10:34:08 -0500

    Add suma funcion

--- cal.cpp
+++ cal.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 using namespace std;
 
+void suma();
+
+
+
+
 int main(){
 
 
 
+
 }
+
+void suma(){
+    int num1;
+    int num2;
+    int num1;
+    int num2;
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+    int resultado = num1 + num2;
+    cout << "La suma es: " << resultado << endl;
+}
+
