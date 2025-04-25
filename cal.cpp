af04799d10469bdda7c7e4a73c2a2e54a8568f8e
Paulina Quishpe <dpquishpe4@espe.edu.ec>
2025-04-25 10:28:38 -0500

    Add resta

--- cal.cpp
+++ cal.cpp
@@ -5,4 +5,32 @@ int main(){
 
 
 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void resta (){
+int num1;
+int num2;
+cout<< "Ingrese el primer numero:";
+cin>> num1;
+cout<< "Ingrese el segundo numero:";
+cin>> num2;
+int resultado =num1 - num2;
+cout<< "La suma es: "<< resultado<< endl;
 }
